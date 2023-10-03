c79d006b33d5177d5c396840ec5891f9536f1b9d
Virous <43663082+VirousMK@users.noreply.github.com>
2023-10-03 11:54:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ input.type = 'text';
 input.name = 'q';
 input.value = decodeURIComponent(get.q);
 
+let input2 = document.createElement('input');
+input2.type = 'text';
+input2.name = 'fast-filter';
+input2.value = 'FEDERATION_MANGA_SUBJECT'
+
 form.appendChild(input);
+form.appendChild(input2);
 document.body.appendChild(form);
 form.submit();
 form.remove();
